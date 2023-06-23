4293c96723001bfafa1e32235d389a398c4c54d2
KIWI\ADMIN <21522217@gm.uit.edu.vn>
2023-06-23 09:30:01 +0700

    finish running AI emotion detection on local web

--- api/app.py
+++ api/app.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    # Video streaming at home page
     return render_template('index.html')
 
 @app.route('/video')
 def video():
+    # Video streaming route. Put this in the src attribute of an img tag.
     return Response(render_emotion_detector(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__=="__main__":
--- api/utilities.py
+++ api/utilities.py
@@ -2,31 +2,28 @@ import cv2
 import numpy as np
 import keras.utils as image
 import warnings
+import time
 warnings.filterwarnings("ignore")
 from keras.utils import img_to_array
 from keras import models
-import matplotlib.pyplot as plt
 
 # load model
-retrievedModel = models.load_model("../api/model.pt")
+retrievedModel = models.load_model("model.pt")
 face_haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
 
 def render_emotion_detector():
-    ################ THE FUNC IS NOT COMPLETE AS IT SHOWS A NEW WINDOW ON CALLING THE WEB
-    ################# NEEDS TO CHANGE WHOLE FUNCTION
-    #################
-    #################
     while True:
-        ret, test_img = cap.read()  # captures frame and returns boolean value and captured image
+        ret, frame = cap.read()  # captures frame and returns boolean value and captured image
         if not ret:
             continue
-        gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(frame, (0, 0), None, 1, 1)
+        gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
 
         for (x, y, w, h) in faces_detected:
-            cv2.rectangle(test_img, (x, y), (x + w, y + h), (255, 0, 0), thickness=7)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), thickness=7)
             roi_gray = gray_img[y:y + w, x:x + h]  # cropping region of interest i.e. face area from  image
             roi_gray = cv2.resize(roi_gray, (224, 224))
             img_pixels = image.img_to_array(roi_gray)
@@ -41,10 +38,12 @@ def render_emotion_detector():
             emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
             predicted_emotion = emotions[max_index]
 
-            cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            cv2.putText(frame, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
-        resized_img = cv2.resize(test_img, (1000, 700))
-        cv2.imshow('Facial emotion analysis ', resized_img)
+        ret, buffer=cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        time.sleep(0.1)
 
         if cv2.waitKey(10) == ord('q'):  # wait until 'q' key is pressed
             break
